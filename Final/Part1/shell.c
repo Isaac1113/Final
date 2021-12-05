7aa32862560427c9a51570d27bd193b89cf592b3
Isaac Diaz <itdiaz@cpp.edu>
2021-12-05 01:53:00 +0000

    Add main

---
+++ Final/Part1/shell.c
@@ -0,0 +1,14 @@
+// Creating a shell for Part 1 of final project
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    //Load config files, if any
+
+    // Run command loop.
+    lsh_loop();
+
+    // Perform any shutdown/cleanup
+
+    return EXIT_SUCCESS;
+}
